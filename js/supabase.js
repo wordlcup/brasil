073517d4ab4884c247aca3feadbdeea39399670b
ti <mguimarcos39@gmail.com>
2026-05-25 16:00:56 -0300

    First Commit

--- js/supabase.js
+++ js/supabase.js
@@ -5,5 +5,6 @@
 const SUPABASE_URL = 'https://kiojjjdgxpnhfubxiuns.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtpb2pqamRneHBuaGZ1YnhpdW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MjUxODksImV4cCI6MjA5NTMwMTE4OX0.aHXWj1NBYLwato4S5nYzFxtDF1yYZfDjjY-PB-4Czcc';
 
-// Inicializa o cliente do Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// O CDN já declara "var supabase". Para não dar erro de "already declared",
+// reatribuímos a variável global com o cliente inicializado.
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
